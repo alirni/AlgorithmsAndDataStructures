f5714a1f3fe5f9f93ff67709cd2e20bdb4577219
ali ramezani <aliramezani19.1@gmail.com>
2020-05-14 07:47:15 +0430

    feat: check 2 string

---
+++ src/check2String2.js
@@ -0,0 +1,27 @@
+const checkIfMatch = (string1, string2) => {
+    if (string1.length !== string2.length) {
+        return false;
+    }
+    
+    const dictionary1 = {};
+    const dictionary2 = {};
+    for (const char of string1) {
+        dictionary1[char] = char;
+    }
+    for (const char in string2) {
+        dictionary2[char] = string2[char];
+    }
+
+    console.log('check2String2.js:15', {
+        dictionary1, dictionary2,});
+
+    for (let i = 0; i < Object.keys(dictionary1).length; i++) {
+        if (!dictionary1[dictionary2[i]]) {
+            return false;
+        }
+    }
+    return true;
+}
+// console.log('--------->', checkIfMatch('abcd', 'dbac'));
+// console.log('--------->', checkIfMatch('abcd', 'dbgc'));
+console.log('--------->', checkIfMatch('abcdc', 'abcdg'));
---
+++ src/checkIfMatch.js
@@ -0,0 +1,37 @@
+const checkIfMatch = (string1, string2) => {
+  if (string1.length !== string2.length) {
+    return false;
+  }
+  
+  const dictionary1 = {};
+  const dictionary2 = {};
+  for (const char of string1) {
+    if (typeof dictionary1[char] === "undefined") {
+      dictionary1[char] = 0;
+    }
+    dictionary1[char]++;
+  }
+  for (const char of string2) {
+    if (typeof dictionary2[char] === "undefined") {
+      dictionary2[char] = 0;
+    }
+    dictionary2[char]++;
+  }
+  // console.log('dictionary1: ', dictionary1);
+  // console.log('dictionary2: ', dictionary2);
+  for (const key in dictionary1) {
+    // console.log('key: ', key);
+    // console.log('dictionary1[key]: ', dictionary1[key]);
+    // console.log('dictionary2[key]: ', dictionary2[key]);
+    
+    if (dictionary1[key] !== dictionary2[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// console.log('\'abcd\', \'dbac\' --------->', checkIfMatch('abcd', 'dbac'));
+// console.log('\'abcd\', \'dbgc\' --------->', checkIfMatch('abcd', 'dbgc'));
+// console.log('\'abcdc\', \'abcdg\' --------->', checkIfMatch('abcdc', 'abcdg'));
+console.log('\'abcdc\', \'abcdg\' --------->', checkIfMatch('abcdc', 'dbhca'));
