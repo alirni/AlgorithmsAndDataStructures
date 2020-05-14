c7ed41c0286b3d5c22212882f7bd42855a0bc397
ali ramezani <aliramezani19.1@gmail.com>
2020-05-14 07:47:52 +0430

    feat(naturalNumbers)

---
+++ src/naturalNumbers.js
@@ -0,0 +1,14 @@
+const computeNaturalNumber = (num1, num2, num3) => {
+  let sum = 0;
+
+  for (let i = 0; i < num3; i++) {
+    if (i % num1 === 0 || i % num2 === 0) {
+      sum = sum + i;
+    }
+  }
+
+  return sum;
+}
+
+console.log('computeNaturalNumber(3, 5, 1000) ====>', computeNaturalNumber(3, 5, 1000));
+console.log('computeNaturalNumber(3, 5, 10) ====>', computeNaturalNumber(3, 5, 10));
