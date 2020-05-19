d5c82cb92db0490f5fd3fa56424fa40e352b00e2
ali ramezani <aliramezani19.1@gmail.com>
2020-05-19 08:00:35 +0430

    fix

--- src/fibonacciNumbers.js
+++ src/fibonacciNumbers.js
@@ -2,7 +2,7 @@
 // 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
 // By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even - valued terms.
 
-function fibonacci() {
+const fibonacci = () => {
   let a = 1,
     b = 0,
     temp;
@@ -21,8 +21,8 @@ function fibonacci() {
       console.log(" >>>> ", b, fiboSum);
     }
   }
-  console.log("fibonacci", fiboSum);
-  return b;
-}
+  return fiboSum;
+};
 
-fibonacci();
+const sum = fibonacci();
+console.log("fibonacci sum", sum);
